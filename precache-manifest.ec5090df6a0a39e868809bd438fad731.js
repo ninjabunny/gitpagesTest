a9822f39c3ff2e22b05a618cd4572d001588b2f1
Andee Tao <andeetao@Andees-iMac.attlocal.net>
2019-05-24 22:22:17 -0700

    Updates

---
+++ precache-manifest.ec5090df6a0a39e868809bd438fad731.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd3a746328efb6e8b18027823e1990d",
+    "url": "/gitpagesTest/index.html"
+  },
+  {
+    "revision": "fe6729bd84290ef2d306",
+    "url": "/gitpagesTest/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/gitpagesTest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fe6729bd84290ef2d306",
+    "url": "/gitpagesTest/static/js/main.ab9a0ac0.chunk.js"
+  },
+  {
+    "revision": "c6e60c70bbd1797684e7",
+    "url": "/gitpagesTest/static/js/runtime~main.54116e4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitpagesTest/static/media/logo.5d5d9eef.svg"
+  }
+]);
